5d11cf8808f02982248b7512cc26e10b02da6a3a
Sndrpxx1 <zhantik201000@gmail.com>
2024-11-11 19:43:09 +0300

    Update pas.py

--- pas.py
+++ pas.py
@@ -1,1 +1,17 @@
-print
+import random
+
+
+def gen_pass(n):
+    elements = "+-/*!&$#?=@<>123456789"
+    password = ""
+
+    for i in range(n):
+        password += random.choice(elements)
+
+    return password
+
+
+print(gen_pass(5))
+
+print(gen_pass(12))
+
